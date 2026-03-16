db4d9330d9bb2e22c7d379c9f30582ef3e7c8361
Ginozzkvs <Ginokvs@gmail.com>
2026-03-16 12:31:43 +0700

    Switch Alpine.js to jsdelivr CDN

--- resources/views/staff/scan.blade.php
+++ resources/views/staff/scan.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-<script src="https://unpkg.com/alpinejs@3.14.9/dist/cdn.min.js" defer></script>
+<script src="https://cdn.jsdelivr.net/npm/alpinejs@3/dist/cdn.min.js" defer></script>
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@400;500;600;700&family=Inter:wght@300;400;500;600;700&display=swap" rel="stylesheet">
